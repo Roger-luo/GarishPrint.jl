b655a5ec9c34d8d9d79ecdf25674513befd07d8b
Roger-Luo <rogerluo.rl18@gmail.com>
2021-06-23 00:24:59 -0400

    add doc build

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter
+using GarishPrint
+using DocThemeIndigo
+
+indigo = DocThemeIndigo.install(GarishPrint)
+
+makedocs(;
+    modules = [GarishPrint],
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical="https://Roger-luo.github.io/GarishPrint.jl",
+        assets=String[indigo],
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Quick Start" => "quick-start.md",
+        "Advanced Usage" => "advance.md",
+        "References" => "ref.md",
+    ],
+    repo = "https://github.com/Roger-luo/GarishPrint.jl",
+    sitename = "GarishPrint.jl",
+)
+
+deploydocs(; repo = "github.com/Roger-luo/GarishPrint.jl")
